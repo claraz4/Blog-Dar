ef2698954d2cb6b464f551b65cb863638f83c1fc
LAPTOP-GO9GN5TP\Lenovo <jibaijoelle@gmail.com>
2024-06-26 16:48:16 +0300

    navigation bar..?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar /> {/* Add the NavBar component here */}
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
