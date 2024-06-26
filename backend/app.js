0c852c95f247ef51ef22953a692c07cda1323ff6
Roni <roni.r.daou@gmail.com>
2024-06-26 16:07:35 +0300

    Fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
 const express = require('express')
+
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+//init app and middleware
+const app = express()
+
+const workoutRoutes = require('./routes/workouts')
+
+const userRoutes = require('./routes/user')
+
+app.use(express.json())
+
+app.use('/workouts', workoutRoutes);
+app.use('/user', userRoutes);
+
+mongoose.connect(process.env.URI)
+    .then(() => {
+        app.listen(process.env.Port, () => {
+            console.log('Listening on port ', process.env.port)
+        })
+    }).catch((error) => {
+        console.log(error)
+    })
