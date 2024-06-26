b3ceaf221e5b180715592c493549f0ae89740e8c
Clara <clarazammar4@gmail.com>
2024-06-26 15:50:53 +0300

    New Changes

---
+++ frontend/src/components/HomeBlogs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import BlogBox from './BlogBox';
+
+export default function HomeBlogs({ title, blogs }) {
+    const [blogsElement, setBlogsElement] = React.useState([]);
+
+    React.useEffect(() => {
+        setBlogsElement(blogs.map((blog) => {
+            return (
+                <BlogBox 
+                    blog={blog}
+                />
+            )
+        }))
+    }, [])
+
+    return (
+        <div className="home-blogs--container">
+            <h6>{props.title}</h6>
+            <div>
+                {blogsElement}
+            </div>
+        </div>
+    )
+}
