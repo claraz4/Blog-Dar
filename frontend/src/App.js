5610d72513aa2abcb7c88e62d6adffd81da67a20
Clara <clarazammar4@gmail.com>
2024-06-26 15:36:28 +0300

    Started with App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
