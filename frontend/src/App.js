7a2538b7893b8a41029bfbcf85ec501ba0b0fc77
Clara <clarazammar4@gmail.com>
2024-06-27 12:41:36 +0300

    Fixed the WriteBlog route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import SingleBlog from "./pages/SingleBlog";
 import NavBar from "./components/NavBar";
 import SignupForm from './pages/SignupForm'; // Adjust the path as per your actual file structure
+import WriteBlog from './pages/WriteBlog';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/blog" element={<SingleBlog />} />
+          <Route exact path="/write" element={<WriteBlog />} />
           <Route exact path="/signup" element={<SignupForm />} /> 
         </Routes>
       </Router>
