e546a6ae915211ab19f35b6e0ae49df45c3265f7
LAPTOP-GO9GN5TP\Lenovo <jibaijoelle@gmail.com>
2024-06-27 12:26:45 +0300

    Sign up form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
+// App.js
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import BlogBox from "./components/BlogBox";
 import SingleBlog from "./pages/SingleBlog";
 import NavBar from "./components/NavBar";
-import WriteBlog from "./pages/WriteBlog";
+import SignupForm from './pages/SignupForm'; // Adjust the path as per your actual file structure
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar /> {/* Add the NavBar component here */}
+        <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/blog" element={<SingleBlog />} />
-          <Route exact path="/write" element={<WriteBlog />} />
+          <Route exact path="/signup" element={<SignupForm />} /> 
         </Routes>
       </Router>
     </div>
